e6c76b67894fc5269f7dcc145ba6cdde4344861a
airi <airi-love-clover@rakuten.jp>
2021-03-28 01:37:18 +0900

    🍱 Add assets.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-pink": "#fac7c777",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
