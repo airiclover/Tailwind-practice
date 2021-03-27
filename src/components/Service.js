8c8cf47c6fe0eb2566f422450253162d2039bfd2
airi <airi-love-clover@rakuten.jp>
2021-03-28 00:24:11 +0900

    💄 Add the UI and style files.

---
+++ src/components/Service.js
@@ -0,0 +1,47 @@
+import Image from "next/image";
+
+const SERVICES = [
+  {
+    src: "/mac.svg",
+    name: "ＨＰ・ＬＰ制作",
+    contents:
+      "ヒアリング内容をもとに、お客さまの目的に沿ったWEB制作を致します。各デバイス幅に合わせてデザインするレスポンジブ化や、サイト修正も対応可能です。",
+  },
+  {
+    src: "/code.svg",
+    name: "コーディング",
+    contents:
+      "HTML・CSSはもちろん、JavaScript(jQuery)を用いてアニメーションなどの動的なコンテンツの対応も可能です。シンプルでわかりやすい、SEO対策まで意識したコーディングをさせていただきます。",
+  },
+  {
+    src: "/create.svg",
+    name: "Writing",
+    contents:
+      "ブログ・ウェブメディア（記事作成）のライティングなどにも対応致します。ご相談下さい。",
+  },
+];
+
+export function Service() {
+  return (
+    <div>
+      <h1 class="font-serif text-3xl tracking-wide">Service</h1>
+      <div>
+        {/* 👇map回す */}
+        {SERVICES.map((service) => (
+          <div>
+            <Image
+              src={service.src}
+              alt={service.name}
+              loading="eager"
+              width={200}
+              height={200}
+              priority
+            />
+            <h2>{service.name}</h2>
+            <p>{service.contents}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Top.js
@@ -0,0 +1,14 @@
+export function Top() {
+  return (
+    <div
+    // class="bg-cover h-48 w-64"
+    // style="background-image: url('https://tailwind.build/placeholders/pictures/office.jpg')"
+    >
+      <div className="font-serif text-4xl">
+        Welcome to
+        <br />
+        my portfolio.
+      </div>
+    </div>
+  );
+}
