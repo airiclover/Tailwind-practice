1e91e1570caad15949982719c6435caba9e313a7
airi <airi-love-clover@rakuten.jp>
2021-03-28 00:26:28 +0900

    ♻️ Refactor code.

--- src/components/Top.js
+++ src/components/Top.js
@@ -2,7 +2,7 @@ export function Top() {
   return (
     <div
     // class="bg-cover h-48 w-64"
-    // style="background-image: url('https://tailwind.build/placeholders/pictures/office.jpg')"
+    // style="background-image: url('')"
     >
       <div className="font-serif text-4xl">
         Welcome to
