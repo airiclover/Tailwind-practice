02e991dce0378795315790863cb9d5de5726e7fc
airi <airi-love-clover@rakuten.jp>
2021-03-28 22:24:29 +0900

    💄 Add the UI files.

---
+++ src/components/Contact.js
@@ -0,0 +1,7 @@
+export function Contact() {
+  return (
+    <div className="py-12 px-2 text-center bg-dark-pink">
+      <h1 className="font-Unna text-3xl tracking-wide">Contact</h1>
+    </div>
+  );
+}
---
+++ src/components/Portfolios.js
@@ -0,0 +1,7 @@
+export function Portfolios() {
+  return (
+    <div className="py-12 px-2 text-center bg-light-yellow">
+      <h1 className="font-Unna text-3xl tracking-wide">Portfolios</h1>
+    </div>
+  );
+}
